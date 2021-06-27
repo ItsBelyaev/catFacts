da8899e7e0c374cf34dcf101a63cdb762d57eab7
down ultra <67225488+ultradown@users.noreply.github.com>
2021-06-27 15:14:57 +0300

    End

---
+++ apiCats/CatManager.swift
@@ -0,0 +1,8 @@
+//
+//  CatManager.swift
+//  apiCats
+//
+//  Created by Daniil Belyaev on 27.06.2021.
+//
+
+import Foundation
---
+++ apiCats/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  apiCats
+//
+//  Created by Daniil Belyaev on 27.06.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
